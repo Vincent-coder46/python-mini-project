cb6d336cf0ab1435d0ecd2ec2a77ff7ff4d013d9
Vincent-coder46 <ayoolajoy@gmail.com>
2026-07-10 00:17:03 +0100

    Word analyzer

---
+++ Word-analyzer/word-analyzer.py
@@ -0,0 +1,16 @@
+course = input("Enter a word or sentence:")
+course = "Machine learning"
+print("\n==========================================")
+print("WORD ANALYZER".center(40))
+print("============================================")
+print("\nEnter a word or sentence: Machine learning")
+print("\n=============================================")
+print(f"Original Text: {course}")
+print(f"Uppercase: {course.upper()}")
+print(f"Lowercase: {course.lower()}")
+print(f"Title Case: {course.title()}")
+print(f"First Letter: {course[0]}")
+print(f"Last Letter: {course[15]}")
+print(f"Characters: {len(course)}")
+
+
